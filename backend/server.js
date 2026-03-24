2e65790f33c768d24e2e5774a1c2a5f589df6a74
DudiBitran <1234dudi@gmail.com>
2026-03-24 18:25:05 +0200

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/", require("./routes/leadRoute"));
+app.use("/api", require("./routes/leadRoute"));
+
+const frontendPath = path.join(__dirname, "../frontend/dist");
+app.use(express.static(frontendPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
